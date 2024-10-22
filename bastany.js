4ab20e44e6880b1833ddf1d64bc2eb823831606e
mamdoos <118680451+mhbemani@users.noreply.github.com>
2024-10-23 00:32:04 +0330

    Create bastany.js
    this is the JavaScript of the home page

---
+++ bastany.js
@@ -0,0 +1,6 @@
+function open_product(){
+    
+    if(document.getElementById("product-bar").isConnected) 
+        document.getElementById("product-bar").style.height = "80dvh"; 
+    else document.getElementById("product-bar").ariaExpanded.height = "10dvh";
+}
